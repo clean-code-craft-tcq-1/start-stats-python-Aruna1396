21175ce3123fc2c26ed1baaea04a748b1206c99d
Aruna Gopalakrishnan <Gopalakrishnan.Aruna@in.bosch.com>
2021-01-24 09:39:17 +0530

    Update statistics functionalities

--- statistics.py
+++ statistics.py
@@ -1,3 +1,14 @@
+import math
+
 
 def calculateStats(numbers):
-  return None
+    computedStatsDict = {}
+    numLength = len(numbers)
+    if numLength == 0:
+        computedStatsDict = dict.fromkeys(["avg", "max", "min"], float(math.nan))
+    else:
+        computedStatsDict["avg"] = sum(numbers) / numLength
+        computedStatsDict["max"] = max(numbers)
+        computedStatsDict["min"] = min(numbers)
+    return computedStatsDict
+
